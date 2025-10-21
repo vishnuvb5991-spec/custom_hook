91c2a68cec6e29a08253813d7bf61a91489ecb8d
vishnuvb5991-spec <vishnuvb5991@gmail.com>
2025-10-21 12:19:12 -0400

    saved data

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,26 @@
+/*import {useState, useEffect } from "react";
+//import { useState } from "react";
+import React, {useState, useEffect } from "react";
 const UseFetch = (url) => {
-    
+    const[data,setData]=useState(); 
+     useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+return [data]
 }
 
-export default UseFetch
+export default <UseFetch></UseFetch>*/
+
+import React, { useEffect, useState } from 'react'
+
+ const useFetch = (url) => {
+    const[data,setData]=useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+  return [data]
+}
+
+export default useFetch;
